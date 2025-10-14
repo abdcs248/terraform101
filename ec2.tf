ee3c3e8932621f0d6271ed1451adb75e0b4bcd74
abdcs248 <135154608+abdcs248@users.noreply.github.com>
2025-10-15 01:57:30 +0500

    some new files

--- ec2.tf
+++ ec2.tf
@@ -55,11 +55,12 @@ resource "aws_instance" "my_instance" {
 
     key_name = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_security_group.name]
-    instance_type = "t3.micro"
-    ami = "ami-0a716d3f3b16d290c"
+    instance_type = var.ec2_instance_type
+    ami = var.ec2_ami_id
+     user_data = file("install_nginx.sh")
 
     root_block_device {
-      volume_size = 15
+      volume_size = var.ec2_root_storage_size
       volume_type = "gp3"
     }
 
